2e18a2bf9613d035f621b94d055ddd2eccf32d9e
YUYING-toro <candy80523@hotmail.com>
2024-07-27 18:35:26 -0400

    edit content to test action

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React CICD, test Action.
+          Learn React CICD, test Action, test2
         </a>
       </header>
     </div>
